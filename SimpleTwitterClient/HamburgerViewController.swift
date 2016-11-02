5aa846b66003cb565ce1f5a491d14c6a66f0cca7
wassupnari <wassupnari@gmail.com>
2016-11-01 23:05:39 -0700

    Added empty views in HamburgerViewController

---
+++ SimpleTwitterClient/HamburgerViewController.swift
@@ -0,0 +1,51 @@
+//
+//  HamburgerViewController.swift
+//  SimpleTwitterClient
+//
+//  Created by Nari Shin on 11/1/16.
+//  Copyright © 2016 Nari Shin. All rights reserved.
+//
+
+import UIKit
+
+class HamburgerViewController: UIViewController {
+
+    @IBOutlet weak var menuView: UIView!
+    @IBOutlet weak var contentView: UIView!
+    
+    @IBOutlet weak var leftMarginConstraint: NSLayoutConstraint!
+    var originalLeftMargin: CGFloat = 0.0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onPanGesture(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        let velocity = sender.velocity(in: view)
+        
+        if sender.state == UIGestureRecognizerState.began {
+            originalLeftMargin = leftMarginConstraint.constant
+        }
+//        else if sender.state == UIGestureRecognizer.didChange(view) {
+//            leftMarginConstraint.constant = originalLeftMargin + translation.x
+//        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
