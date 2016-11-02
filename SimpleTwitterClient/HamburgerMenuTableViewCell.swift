6c50c3b867d4a7ea902ad58c8b1354de2c33ccd3
wassupnari <wassupnari@gmail.com>
2016-11-01 22:38:59 -0700

    Added empty viewControllers and navigationControllers

---
+++ SimpleTwitterClient/HamburgerMenuTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HamburgerMenuTableViewCell.swift
+//  SimpleTwitterClient
+//
+//  Created by Nari Shin on 11/1/16.
+//  Copyright © 2016 Nari Shin. All rights reserved.
+//
+
+import UIKit
+
+class HamburgerMenuTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
