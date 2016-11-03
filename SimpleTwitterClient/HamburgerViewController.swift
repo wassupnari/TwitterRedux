adef561c7c570203439c733819edc483ebdcb47f
wassupnari <wassupnari@gmail.com>
2016-11-02 23:30:07 -0700

    Added snap animation

--- SimpleTwitterClient/HamburgerViewController.swift
+++ SimpleTwitterClient/HamburgerViewController.swift
@@ -40,6 +40,14 @@ class HamburgerViewController: UIViewController {
             leftMarginConstraint.constant = originalLeftMargin + translation.x
         } else if sender.state == UIGestureRecognizerState.ended {
             
+            UIView.animate(withDuration: 0.3, animations: {
+                if velocity.x > 0 {
+                    self.leftMarginConstraint.constant = self.view.frame.size.width - 50
+                } else {
+                    self.leftMarginConstraint.constant = 0
+                }
+                self.view.layoutIfNeeded()
+            })
         }
     }
 
